284163749e87379e20a1c9695aa7fa3186eb15d4
omro0107 <142796898+omro0107@users.noreply.github.com>
2025-05-21 18:43:10 +0200

    added back the deleted images, and changed api config

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,20 +1,6 @@
-const apiKey = import.meta.env.VITE_NOROFF_API_KEY;
-
-if (!apiKey) {
-  console.error('API Key is missing! Make sure VITE_NOROFF_API_KEY is set in your environment variables.');
-}
-
 export const API_CONFIG = {
   headers: {
     'Content-Type': 'application/json',
-    'X-Noroff-API-Key': apiKey
+    'X-Noroff-API-Key': import.meta.env.VITE_NOROFF_API_KEY
   }
 };
-
-// Log config in development
-if (import.meta.env.DEV) {
-  console.log('API Config:', {
-    hasApiKey: !!apiKey,
-    baseHeaders: Object.keys(API_CONFIG.headers)
-  });
-}
